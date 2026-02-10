6fe2f7d68181f58087c2425afe1282ef462b071e
Alessio Carone <littlemad@gmail.com>
2026-02-10 13:44:43 +0100

    adding icons and svg spriting functionality and cache busting for the sprites

---
+++ src/app/component/_shared/icon/icon.ts
@@ -0,0 +1,16 @@
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: '[icon]',
+    standalone: true,
+    templateUrl: './icon.html',
+    encapsulation: ViewEncapsulation.None
+})
+export class IconComponent {
+    @Input() name = '';
+    @Input() altText = '';
+
+    /** Cache-busted sprite URL (one random per page load) */
+    private static readonly spriteUrl = `/img/icons.svg?v=${Math.random()}`;
+    readonly url = IconComponent.spriteUrl;
+}
--- src/app/component/_shared/shared-imports.ts
+++ src/app/component/_shared/shared-imports.ts
@@ -1,9 +1,11 @@
 import { GoTopComponent } from './go-top/go-top';
 import { ListComponent } from './list/list';
+import { IconComponent } from './icon/icon';
 import { CommonModule } from '@angular/common';
 
 export const SHARED_IMPORTS = [
     CommonModule,
     GoTopComponent,
+    IconComponent,
     ListComponent,
 ];
